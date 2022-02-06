7e90f5f65cd5675a5f3bd4daf7dd300c205e7a89
Arthur Cosentino <arthurcose@gmail.com>
2022-02-06 18:24:08 -0500

    Allow grass to grow lower

--- scripts/minecraft/block_behaviors/grass_block.lua
+++ scripts/minecraft/block_behaviors/grass_block.lua
@@ -7,7 +7,7 @@ local function update(world, int_x, int_y, int_z)
 
   local spread_x = int_x + math.random(3) - 2
   local spread_y = int_y + math.random(3) - 2
-  local spread_z = int_z + (math.random(3) - 2) * world.layer_diff
+  local spread_z = int_z + (math.random(4) - 3) * world.layer_diff
 
   if world:get_block(spread_x, spread_y, spread_z) == Blocks.DIRT and world:get_block(spread_x, spread_y, spread_z + world.layer_diff) == Blocks.AIR then
     world:set_block(spread_x, spread_y, spread_z, Blocks.GRASS_BLOCK)
