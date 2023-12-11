e534a0942f8d0769d4a2d9ae8516a8b7c1600778
Arthur Cosentino <arthurcose@gmail.com>
2023-12-11 03:42:28 -0500

    Update direction lib

--- scripts/libs/direction.lua
+++ scripts/libs/direction.lua
@@ -1,23 +1,23 @@
 local Direction = {
-  UP = "Up",
-  LEFT = "Left",
-  DOWN = "Down",
-  RIGHT = "Right",
-  UP_LEFT = "Up Left",
-  UP_RIGHT = "Up Right",
-  DOWN_LEFT = "Down Left",
-  DOWN_RIGHT = "Down Right",
+  UP = "UP",
+  LEFT = "LEFT",
+  DOWN = "DOWN",
+  RIGHT = "RIGHT",
+  UP_LEFT = "UP LEFT",
+  UP_RIGHT = "UP RIGHT",
+  DOWN_LEFT = "DOWN LEFT",
+  DOWN_RIGHT = "DOWN RIGHT",
 }
 
 local reverse_table = {
-  ["Up"] = "Down",
-  ["Left"] = "Left",
-  ["Down"] = "Up",
-  ["Right"] = "Left",
-  ["Up Left"] = "Down Right",
-  ["Up Right"] = "Down Left",
-  ["Down Left"] = "Up Right",
-  ["Down Right"] = "Up Left",
+  [Direction.UP] = Direction.DOWN,
+  [Direction.LEFT] = Direction.LEFT,
+  [Direction.DOWN] = Direction.UP,
+  [Direction.RIGHT] = Direction.LEFT,
+  [Direction.UP_LEFT] = Direction.DOWN_RIGHT,
+  [Direction.UP_RIGHT] = Direction.DOWN_LEFT,
+  [Direction.DOWN_LEFT] = Direction.UP_RIGHT,
+  [Direction.DOWN_RIGHT] = Direction.UP_LEFT,
 }
 
 function Direction.reverse(direction)
@@ -49,20 +49,20 @@ function Direction.diagonal_from_points(point_a, point_b)
 end
 
 local directions = {
-  "Up Left",
-  "Up",
-  "Up Right",
-  "Right",
-  "Down Right",
-  "Down",
-  "Down Left",
-  "Left",
+  Direction.UP_LEFT,
+  Direction.UP,
+  Direction.UP_RIGHT,
+  Direction.RIGHT,
+  Direction.DOWN_RIGHT,
+  Direction.DOWN,
+  Direction.DOWN_LEFT,
+  Direction.LEFT,
 }
 
 function Direction.from_offset(x, y)
   local angle = math.atan(y, x)
-  local direction_index = math.floor(angle / math.pi * 4) + 5
-  return directions[direction_index]
+  local direction_index = math.floor(angle / math.pi * 4 + 4.5)
+  return directions[direction_index % 8 + 1]
 end
 
 function Direction.diagonal_from_offset(x, y)
@@ -83,17 +83,58 @@ function Direction.diagonal_from_offset(x, y)
   end
 end
 
+local chebyshev_vectors = {
+  [Direction.UP] = { x = -1, y = -1 },
+  [Direction.LEFT] = { x = -1, y = 1 },
+  [Direction.DOWN] = { x = 1, y = 1 },
+  [Direction.RIGHT] = { x = 1, y = -1 },
+  [Direction.UP_LEFT] = { x = -1, y = 0 },
+  [Direction.UP_RIGHT] = { x = 0, y = -1 },
+  [Direction.DOWN_LEFT] = { x = 0, y = 1 },
+  [Direction.DOWN_RIGHT] = { x = 1, y = 0 },
+}
+
+function Direction.vector(direction)
+  local vector = chebyshev_vectors[direction]
+
+  if vector then
+    return { x = vector.x, y = vector.y }
+  end
+end
+
+local deg45radians = math.sin(math.pi / 4)
+local unit_vectors = {
+  [Direction.UP] = { x = -deg45radians, y = -deg45radians },
+  [Direction.LEFT] = { x = -deg45radians, y = deg45radians },
+  [Direction.DOWN] = { x = deg45radians, y = deg45radians },
+  [Direction.RIGHT] = { x = deg45radians, y = -deg45radians },
+  [Direction.UP_LEFT] = { x = -1, y = 0 },
+  [Direction.UP_RIGHT] = { x = 0, y = -1 },
+  [Direction.DOWN_LEFT] = { x = 0, y = 1 },
+  [Direction.DOWN_RIGHT] = { x = 1, y = 0 },
+}
+
+function Direction.unit_vector(direction)
+  local vector = unit_vectors[direction]
+
+  if vector then
+    return { x = vector.x, y = vector.y }
+  end
+end
+
+-- todo: replace get_point_ahead or add to main library
+
 local deg_45 = math.sin(math.pi / 4.0)
 
 function Direction.get_point_ahead(point, direction, distance)
   local new_point = { x = point.x, y = point.y, z = point.z }
 
   if direction == Direction.UP_LEFT then
-     new_point.x = point.x - distance
-     new_point.y = point.y
-    elseif direction == Direction.DOWN_RIGHT then
-      new_point.x = point.x + distance
-      new_point.y = point.y
+    new_point.x = point.x - distance
+    new_point.y = point.y
+  elseif direction == Direction.DOWN_RIGHT then
+    new_point.x = point.x + distance
+    new_point.y = point.y
   elseif direction == Direction.UP_RIGHT then
     new_point.x = point.x
     new_point.y = point.y - distance
